a49386ce77f5978ff17ddab457a17c0ee6548d4a
tunaozturk <tunoz.23@yahoo.com>
2025-03-28 01:04:33 +0300

    Made changes for macOS env.

--- src/Main.cpp
+++ src/Main.cpp
@@ -32,14 +32,20 @@ int main() {
     // Set error callback
     glfwSetErrorCallback(error_callback);
      // OpenGL 4.2 Core Profile for other systems
+
+    #ifdef __APPLE__
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #else
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	
-    #ifdef __APPLE__
-	    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
     #endif
-    
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     // Create window
     GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL GLFW Window", NULL, NULL);
     if (!window) {
